2e8da5e49e6085d96e9f6f2757b51414682e00ba
David Forer <david.j.forer@gmail.com>
2019-08-29 05:27:06 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -15,11 +15,13 @@ class OwnersController < ApplicationController
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
+    
     redirect "owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
+    @Pets = Pet.all
     erb :'/owners/edit'
   end
 
